80eebe13d91f37e46e14008f03b8683d2fceb5e4
Rahul <rahul.chelmeti@gmail.com>
2024-10-25 18:06:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-0ddc798b3f1a5117e"
     instance_type = "t2.micro"
     key_name = "keypair-rc"
-    security_groups = [aws_security_group.web_server.id]
 }
    
 
